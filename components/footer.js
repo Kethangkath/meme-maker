0d4ea2461a173fc2f9bfbe4d44c92828278549b9
Kenneth Kath <kethangkenneth9@gmail.com>
2023-04-07 16:37:11 +0530

    second commit

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
-const footer = () => {
+const Footer = () => {
+  const footerStyle = {
+    flexShrink: 0,
+    marginTop: 'auto',
+    backgroundColor: "#f2f2f2",
+    color: "#333",
+    padding: "5px 0",
+    textAlign: "center"
+  }
+
   return (
-   <>
-    <div>
-       <p className='d-flex justify-content-center sticky-bottom'> &#169;</p>
-    </div>
-       </>
+    <footer style={footerStyle}>
+      <p>&#169;</p>
+    </footer>
   )
 }
 
-export default footer
+export default Footer
